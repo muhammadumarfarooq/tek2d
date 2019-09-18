271116e497c8a5088a6fd2aa75afca9f585fa67a
tek2d <sami@tek2d.com>
2019-09-18 15:27:04 +0500

    completing whoWeAre with background of particlesjs

---
+++ src/components/WhoWeAre/Particles/Dots.js
@@ -0,0 +1,36 @@
+import React from 'react';
+// import "./Comps.css";
+import Particles from 'react-particles-js';
+
+
+const Comps = () => {
+  return(
+    <div style={{zIndex: -1}}>
+    <Particles
+       params={{
+           "particles": {"number":{"value":30,"density":{"enable":true,"value_area":800}},"color": {
+             "value": ["#3224e9","#ff4200","#8cffed","#FED373"]
+           },"shape":{"type":"polygon","stroke":{"width":0,"color":"#000"},"polygon":{"nb_sides":6},"image":{"src":"","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":5,"random":true,"anim":{"enable":true,"speed":5,"size_min":40,"sync":false}},"line_linked":{"enable":false,"distance":200,"color":"#ffffff","opacity":1,"width":0},"move":{"enable":true,"speed":2,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
+           "interactivity": {
+               "events": {
+                   "onhover": {
+                       "enable": true,
+                       "mode": "repulse"
+                   }
+               }
+           }
+       }}
+       style={{
+               width: '100%',
+               height: '50vh',
+               background: `#f4f6fd`,
+               position: 'absolute'
+             }}
+             />
+ </div>
+  );
+}
+
+
+
+export default Comps;
