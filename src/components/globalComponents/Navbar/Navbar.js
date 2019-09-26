3743a2357b2b06b4570d048d116da680e5160a3b
Muhammad Umar Farooq <develop.umar@gmail.com>
2019-09-26 14:43:46 +0500

    making full responsive navbar

--- src/components/globalComponents/Navbar/Navbar.js
+++ src/components/globalComponents/Navbar/Navbar.js
@@ -3,36 +3,72 @@ import "./Navbar.css";
 
 import logo from "../../../assets/logo.png";
 
-const Navbar = () => {
-  return (
-    <nav id='main-nav'>
-      <div class='container'>
-        <img src={logo} alt='TEK2D' class='logo' />
-        <ul>
-          <li id='chek'>
-            <a id='current' href='index.html'>
-              Home
-            </a>
+class Navbar extends React.Component {
+  state = {
+    showMobileNav: false,
+    links: [
+      { linkTitle: "About Us" },
+      { linkTitle: "Services +" },
+      { linkTitle: "Our Team" },
+      { linkTitle: "Portfolio" },
+      { linkTitle: "Contact Us" }
+    ]
+  };
+
+  bigNavbarLinks = this.state.links.map(link => (
+    <li id='chek'>
+      <a id='current' href='index.html'>
+        {link.linkTitle}
+      </a>
+    </li>
+  ));
+  callMe = () => {
+    this.setState({ showMobileNav: !this.state.showMobileNav });
+  };
+
+  render() {
+    return (
+      <nav id='main-nav'>
+        <div class='container'>
+          <img src={logo} alt='TEK2D' class='logo' />
+          <ul className='bigMenu'>{this.bigNavbarLinks}</ul>
+
+          <div class='div12'>
+            <input
+              class='menu-btn'
+              onChange={this.callMe}
+              type='checkbox'
+              id='menu-btn'
+            />
+            <label class='menu-icon' for='menu-btn'>
+              <span class='navicon'></span>
+            </label>
+          </div>
+        </div>
+        <ul
+          class='menu'
+          style={
+            this.state.showMobileNav
+              ? { maxHeight: "260px" }
+              : { maxHeight: "0" }
+          }
+        >
+          <li>
+            <a href='#work'>Our Work</a>
           </li>
           <li>
-            <a href='about.html'>About Us</a>
+            <a href='#about'>About</a>
           </li>
           <li>
-            <a href='about.html'>Services +</a>
+            <a href='#careers'>Careers</a>
           </li>
           <li>
-            <a href='about.html'>Our Team</a>
-          </li>
-          <li>
-            <a href='about.html'>Portfolio</a>
-          </li>
-          <li>
-            <a href='about.html'>Contact Us</a>
+            <a href='#contact'>Contact</a>
           </li>
         </ul>
-      </div>
-    </nav>
-  );
-};
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
